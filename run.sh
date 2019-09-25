aef95bf69ded401b31ae928c127565c5c0cee5f1
Alex Mickelson <alexmickelson96@gmail.com>
2019-09-25 17:52:00 -0600

    dark theme works now

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 export JEKYLL_VERSION=3.8
-docker run --rm \
+docker run \
   --volume="$PWD:/srv/jekyll" \
   -p 4000:4000 \
+  --name jekyll_run \
   -it jekyll/jekyll:$JEKYLL_VERSION \
   jekyll serve
